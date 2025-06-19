05c448ed521afe9b889e788ec04201fd8eb380ce
paramasivan <paramasivan645@gmail.com>
2025-06-19 17:01:24 +0530

    Add up helmet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Home } from "./pages/Home"
 import Navbar from "./components/Navbar"
 import { Livewire } from "./pages/Livewire"
 import { Synergy } from "./pages/Synergy"
+import Carousel from "./components/Carousel"
+import Footer from "./Footer"
 
 function App() {
  
@@ -10,11 +12,13 @@ function App() {
   return (
     <BrowserRouter>
     <Navbar />
+    <Carousel />
     <Routes>
       <Route path="/" element={<Home />}></Route>  
       <Route path="/lw" element={<Livewire />} > </Route>
       <Route path="/sy" element={<Synergy />}></Route>
     </Routes>
+    <Footer />
     </BrowserRouter>
   
   )
--- client/src/pages/Livewire.jsx
+++ client/src/pages/Livewire.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Livewire = () => {
   return (
-    <div>Livewire</div>
+    <div>
+       <Helmet>
+        <title>CADD Centre Tenkasi - Best CAD Training Institute</title>
+        <meta name="description" content="Join CADD Centre Tenkasi for industry-leading CAD, CAM, and CAE training with expert instructors and placement support." />
+        <meta name="keywords" content="CADD Centre, CAD Courses, Tenkasi, AutoCAD, Revit, SolidWorks" />
+      </Helmet>
+    </div>
   )
 }
